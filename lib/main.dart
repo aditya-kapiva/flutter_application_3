035639e147f9dab7f9a3d11c40a2b8724041b0f4
Aditya Rathore <aditya@kapivaa.in>
2023-08-01 16:59:41 +0530

    v1.1.1v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MainApp());
@@ -8,7 +9,14 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
+  void initState() {
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var x = "Fri, 28 Jul 2023 20:10:17 +0000";
+    print(DateFormat('EEE, dd MMM yyyy hh:mm:ss Z').parse(x,true).toLocal());
     return const MaterialApp(
       home: Scaffold(
         body: Center(
